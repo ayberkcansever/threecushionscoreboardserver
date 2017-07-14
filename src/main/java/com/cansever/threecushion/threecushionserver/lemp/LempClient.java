e381367bbfeeb7cb38138e0881b5837d835f90f2
Ayberk Cansever <ayberkcansever@Ayberks-MacBook-Pro.local>
2017-07-14 13:54:23 +0300

    Lemp integration completed some logs added

---
+++ src/main/java/com/cansever/threecushion/threecushionserver/lemp/LempClient.java
@@ -0,0 +1,11 @@
+package com.cansever.threecushion.threecushionserver.lemp;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by ayberkcansever on 13/07/2017.
+ */
+@Component
+public class LempClient {
+
+}
---
+++ src/main/java/com/cansever/threecushion/threecushionserver/lemp/LempMessage.java
@@ -0,0 +1,7 @@
+package com.cansever.threecushion.threecushionserver.lemp;
+
+/**
+ * Created by ayberkcansever on 13/07/2017.
+ */
+public class LempMessage {
+}
